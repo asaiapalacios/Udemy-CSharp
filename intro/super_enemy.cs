3cf2295f8d8a1d6387200d234ad880ce489ed70d
Asaia Palacios <asaia.palacios@gmail.com>
2017-03-10 15:11:39 -0800

    Extend a class and its behavior(s) through inheritance

---
+++ intro/super_enemy.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+
+// SuperEnemy inherits all the behavior of the Enemy class + it is adding its own behavior
+public class SuperEnemy : Enemy { 
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("hola mundo");
+	}
+}
